0e819055c8fb98cb5f009241f3e37328b42535a4
Luis Martinez <A01748080@tec.mx>
2024-06-07 13:48:53 -0600

    Actualizar página principal con nuevas alertas y estilos

--- js/tiempo.js
+++ js/tiempo.js
@@ -1,20 +1,40 @@
 
 var temporizadorElemento = document.getElementById('temporizador');
-var tiempoRestante = 60;
+var tiempoRestante = 4;
+var contenidoPrincipal = document.getElementsByTagName("main")[0];
 
 var temporizador = setInterval(function() {
-    // Formatear los segundos en el formato MM:SS
     var minutos = Math.floor(tiempoRestante / 60);
     var segundos = tiempoRestante % 60;
     var tiempoFormateado = minutos.toString().padStart(2, '0') + ':' + segundos.toString().padStart(2, '0');
 
-    // Mostrar el tiempo restante en el elemento del temporizador
     temporizadorElemento.textContent = tiempoFormateado;
 
-    // Reducir el tiempo restante en 1 segundo
     tiempoRestante--;
     if (tiempoRestante < 0) {
         clearInterval(temporizador);
         temporizadorElemento.textContent = '¡Tiempo agotado!';
+        fondoDifuminado.style.display = "block";
+        contenidoPrincipal.classList.add("opaco");
+        document.getElementById("miAlerta").style.display = "block";
     }
 }, 1000);
+
+
+function cerrarAlertaPersonalizada() {
+    var fondoDifuminado = document.getElementById("fondoDifuminado");
+    var contenidoPrincipal = document.getElementsByTagName("main")[0];
+
+    fondoDifuminado.style.display = "none";
+    contenidoPrincipal.classList.remove("opaco");
+    document.getElementById("miAlerta").style.display = "none";
+}
+
+function cerrarAlertaPersonalizadVictoria() {
+    var fondoDifuminado = document.getElementById("fondoDifuminado");
+    var contenidoPrincipal = document.getElementsByTagName("main")[0];
+
+    fondoDifuminado.style.display = "none";
+    contenidoPrincipal.classList.remove("opaco");
+    document.getElementById("miAlerta-c").style.display = "none";
+}
